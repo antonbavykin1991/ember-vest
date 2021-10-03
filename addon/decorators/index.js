66fb7c1a352d65f66805f555dfcf4d82500ac402
Volodymyr Radchenko <rreckonerr@gmail.com>
2021-10-03 11:19:04 +0200

    REFACT: move decorators to a separate folder

---
+++ addon/decorators/index.js
@@ -0,0 +1,31 @@
+import { create } from 'vest';
+
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export function Validator(name, fn) {
+  const validate = create(name, fn);
+
+  return function (target) {
+    return class VestValidatorClass extends target {
+      @tracked _validator = validate.get();
+
+      get validator() {
+        return this._validator;
+      }
+
+      get isInvalid() {
+        return !!this.validator.errorCount;
+      }
+
+      get errors() {
+        return this.validator.getErrors();
+      }
+
+      @action
+      validate(...args) {
+        this._validator = validate(this, ...args);
+      }
+    };
+  };
+}
